8066b60410a511bea8fec64160613224443cf35a
lovebai <1507820372@qq.com>
2025-08-24 12:40:07 +0800

    添加环境变量示例文件和更新配置

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const API_HOST = process.env.API_HOST || 'http://127.0.0.1:8001';
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  distDir: 'build',
   async rewrites() {
     return [
       {
